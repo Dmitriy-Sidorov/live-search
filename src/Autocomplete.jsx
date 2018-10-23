12647ab355984508a04a1b1ae02546b0e6b70c86
Dmitriy <>
2018-10-23 20:31:59 +0300

    transfer function, cleaning code

--- src/Autocomplete.jsx
+++ src/Autocomplete.jsx
@@ -1,25 +1,24 @@
 import React, {Component} from "react";
 
-
 const city = ["Москва", "москвская обл", "Рязань", "ростов", "растов", "Тверь", "Великий Новгород", "Казань"];
-
-
-function searchingFor(value) {
-    return function (item) {
-        return item.toLowerCase().substring(0, value.length) === value.toLowerCase().substring(0, value.length);
-    }
-}
 
 class Autocomplete extends Component {
     state = {
         city: city
     };
 
+    searchingFor = (value) => {
+        return (city) => {
+            console.log(city);
+            return city.toLowerCase().substring(0, value.length) === value.toLowerCase().substring(0, value.length);
+        }
+    }
+
     render() {
         const {city} = this.state;
         return (
             <div className="podskazka">
-                {city.filter(searchingFor(this.props.value)).map((city, index) => (
+                {city.filter(this.searchingFor(this.props.value)).map((city, index) => (
                     <div key={index}>{city}</div>
                 ))}
             </div>
